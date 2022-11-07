7664af2bd4750a7599c1c9f6cfe4b750cd4a4992
enriiquee <94898081+Enriiqueee@users.noreply.github.com>
2022-11-07 18:54:46 +0100

    push sin errores

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,20 @@
 public class Main {
     public static void main(String[] args) {
+        AnimalPrinter animalPrinter = new AnimalPrinter();
+
         Dog dog = new Dog();
         dog.setName("Peluche");
         dog.setAge(8);
-        animalPrinter.printAnimal(dog);
+        animalPrinter.printAnimals(dog);
 
         Horse horse = new Horse();
         horse.setName("Carlos");
         horse.setAge(2);
-        animalPrinter.printAnimal(horse);
+        animalPrinter.printAnimals(horse);
 
         Whale whale = new Whale();
         whale.setName("Paula");
         whale.setAge(3);
-        animalPrinter.printAnimal(whale);
+        animalPrinter.printAnimals(whale);
     }
 }
